93bffc81816075673f217bfbf0c460b1414679c6
cyril <cyril.waller@citrin.ch>
2017-09-25 11:28:06 +0200

    Added little Helpers for User for confirming message. Seekbar implemented

--- app/src/main/java/ch/nedlox/chattermatter/ConfirmMessage.java
+++ app/src/main/java/ch/nedlox/chattermatter/ConfirmMessage.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.R.drawable.ic_media_pause;
+import static android.R.drawable.ic_media_play;
 
 /**
  * Created by cyril on 25.09.17.
@@ -17,6 +23,7 @@ public class ConfirmMessage extends AppCompatActivity {
 
     MediaPlayer mediaPlayer ;
     FloatingActionButton play_bttn;
+    SeekBar seekBar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +44,39 @@ public class ConfirmMessage extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                final int duration = mediaPlayer.getDuration();
+                final int amoungToupdate = duration / 100;
+                Timer mTimer = new Timer();
+                mTimer.schedule(new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                if (!(amoungToupdate * seekBar.getProgress() >= duration)) {
+                                    int p = seekBar.getProgress();
+                                    p += 1;
+                                    seekBar.setProgress(p);
+                                }
+                            }
+                        });
+                    };
+                }, amoungToupdate);
 
                 mediaPlayer.start();
                 Toast.makeText(ConfirmMessage.this, "Recording Playing",
                         Toast.LENGTH_LONG).show();
+                play_bttn.setImageResource(ic_media_pause);
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        play_bttn.setImageResource(ic_media_play);
+                    }
+                });
+
             }
         });
 
