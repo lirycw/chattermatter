dff03282a3f645268e0d19d288215db91e768ee5
lirycw <lirycw@users.noreply.github.com>
2018-03-07 20:18:40 +0100

    added new layout base

--- app/src/main/java/ch/nedlox/chattermatter/MainActivity.java
+++ app/src/main/java/ch/nedlox/chattermatter/MainActivity.java
@@ -14,11 +14,16 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -30,9 +35,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.content.Context;
+
 
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -62,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.content_main);
 
-    MessageList = (ListView) findViewById(R.id.messageListView);
+    MessageList = (ListView) findViewById(R.id.listview);
 
     request = getJson();
 
@@ -205,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
         temp.add(al.get(i).getDate().toString());
       }
 
-      ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, temp);
+      ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_view, R.id.list_item_text, temp);
       MessageList.setAdapter(adapter);
     } catch (JSONException e) {
       e.printStackTrace();
@@ -238,5 +246,87 @@ public class MainActivity extends AppCompatActivity {
 
     return request;
   }
+
+    private class MyListAdaper extends ArrayAdapter<String> {
+        private int layout;
+        private List<String> mObjects;
+
+        private MyListAdaper(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ViewHolder mainViewholder = null;
+            if (convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.list_item_thumbnail);
+                viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
+                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_btn);
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (ViewHolder) convertView.getTag();
+            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+                MediaPlayer mediaPlayer;
+                String AudioSavePathInDevice = null;
+                int playcounter = 0;
+                public void onClick(View v) {
+                    if (playcounter == 0){
+                        playcounter = 1;
+                        String value;
+                        String voice = al.get(position).getVoice();
+                        byte[] decoded = Base64.decode(voice, 0);
+                        try {
+                            AudioSavePathInDevice =
+                                    Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                                            CreateRandomAudioFileName(5) + "AudioRecording.3gp";
+                            final File file2 = new File(AudioSavePathInDevice);
+                            FileOutputStream os = new FileOutputStream(file2, true);
+                            os.write(decoded);
+                            os.close();
+                            mediaPlayer = new MediaPlayer();
+                            try {
+                                mediaPlayer.setDataSource(AudioSavePathInDevice);
+                                mediaPlayer.prepare();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            mediaPlayer.start();
+                            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                                @Override
+                                public void onCompletion(MediaPlayer mp) {
+                                    playcounter = 0;
+                                    file2.delete();
+                                }
+                            });
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }else {
+                        mediaPlayer.stop();
+                        playcounter = 0;
+
+                    }
+                    Toast.makeText(getContext(), "Button was clicked for list item " + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+            mainViewholder.title.setText(getItem(position));
+
+            return convertView;
+        }
+    }
+
+    public class ViewHolder {
+
+        ImageView thumbnail;
+        TextView title;
+        Button button;
+    }
 
 }
